1e71f636aef90f9c6f51c45d213ff10c2e21f3b2
nymulislam <naymulislam241@gmail.com>
2023-11-09 23:49:02 +0600

    updated server

--- index.js
+++ index.js
@@ -1,13 +1,40 @@
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
 require('dotenv').config();
 const app = express();
+const jwt = require('jsonwebtoken')
+
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
+
+
+
+const getUserIdFromAuthentication = req => {
+    const authHeader = req.headers.authorization;
+  
+    if (!authHeader || !authHeader.startsWith('Bearer')) {
+      return null;
+    }
+  
+    const token = authHeader.split(" ")[1];
+    const decodedToken = jwt.verify(token, process.env.Access_Token_Key);
+  
+    return decodedToken && decodedToken.userId ? decodedToken.userId : null;
+  }
+  
+
+app.use((req, res, next) => {
+    req.userId = getUserIdFromAuthentication(req);
+    next();
+})
+
+  
 
 const uri = `mongodb+srv://${process.env.DB_User}:${process.env.DB_Pass}@cluster0.tloczwa.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -26,6 +53,14 @@ async function run() {
         await client.connect();
 
         const jobsCollection = client.db("jobsDB").collection("jobs");
+
+
+        // // post related api
+        // app.post ('/jwt', async(req, res) => {
+        //     const user = req.body;
+        //     const token = jwt.sign(user, process.env.Access_Token_Key, {expiresIn: "100h"});
+        //     res.send({token})
+        // })
 
         // create data
         app.post('/jobs', async (req, res) => {
@@ -50,45 +85,46 @@ async function run() {
             res.send(result)
         })
 
-        //update data
-        app.put('/jobs/:id', async (req, res) => {
-            const id = req.params.id;
-            const query = { _id: new ObjectId(id) };
-            const options = { upsert: true };
-            const updateJob = req.body;
-            const job = {
-                $set: {
-                    user: updateJob.user,
-                    title: updateJob.title,
-                    type: updateJob.type,
-                    salary: updateJob.salary,
-                    applicant: updateJob.applicant,
-                    description: updateJob.description,
-                    url: updateJob.url,
-                    date: updateJob.date,
-                    deadline: updateJob.deadline,
-                }
-            }
-            const result = await jobsCollection.updateOne(query, job, options)
-            res.send(result)
-        })
+        // //update data
+        // app.put('/jobs/:id', async (req, res) => {
+        //     const id = req.params.id;
+        //     const query = { _id: new ObjectId(id) };
+        //     const options = { upsert: true };
+        //     const updateJob = req.body;
+        //     const job = {
+        //         $set: {
+        //             user: updateJob.user,
+        //             title: updateJob.title,
+        //             type: updateJob.type,
+        //             salary: updateJob.salary,
+        //             applicant: updateJob.applicant,
+        //             description: updateJob.description,
+        //             url: updateJob.url,
+        //             date: updateJob.date,
+        //             deadline: updateJob.deadline,
+        //         }
+        //     }
+        //     const result = await jobsCollection.updateOne(query, job, options)
+        //     res.send(result)
+        // })
 
-        // delete data
-        app.delete('/jobs/:id', async (req, res) => {
-            const id = req.params.id;
-            const query = { _id: new ObjectId(id) }
-            const result = await jobsCollection.deleteOne(query);
-            res.send(result)
-        })
+        // // delete data
+        // app.delete('/jobs/:id', async (req, res) => {
+        //     const id = req.params.id;
+        //     const query = { _id: new ObjectId(id) }
+        //     const result = await jobsCollection.deleteOne(query);
+        //     res.send(result)
+        // })
 
-        
+
         //All Jobs
         const allJobsCollection = client.db("jobsDB").collection("allJobs")
 
-        // add cart
+        // add Job
         app.post('/allJobs/add', async (req, res) => {
-            const selectedCar = req.body;
-            const result = await allJobsCollection.insertOne(selectedCar);
+            const selectedJob = req.body;
+            selectedJob.userId = req.userId;
+            const result = await allJobsCollection.insertOne(selectedJob);
             res.send(result)
         })
 
@@ -107,10 +143,30 @@ async function run() {
             res.send(result)
         })
 
+
+        // get data specific user
+        app.get('/myJobs/:userId', async (req, res) => {
+            try {
+                const userId = req.params.userId;
+                console.log("Fetching user jobs for userId:", userId);
+
+                const cursor = allJobsCollection.find({ user: userId });
+                const result = await cursor.toArray();
+
+                console.log("User jobs fetched successfully:", result);
+
+                res.json(result);
+            } catch (error) {
+                console.error("Error fetching user jobs:", error);
+                res.status(500).json({ error: "Internal Server Error" });
+            }
+        });
+
+
         // delete car from cart
         app.delete('/cart/:id', async (req, res) => {
             const id = req.params.id;
-            const query = {_id: new ObjectId(id)};
+            const query = { _id: new ObjectId(id) };
             const result = await allJobsCollection.deleteOne(query)
             res.send(result);
         })
